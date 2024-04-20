9edabb070b17c02bf0ea7c9e799506992e6f5590
Thaynix <thaygoncalvesjg@gmail.com>
2024-04-20 12:35:13 -0300

    :sparkles: feat: adiciona página de usuario e página de carrinho

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('usuario/', user, name='user'),
+    path('carrinho/', carrinho, name='carrinho'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def user(request):
+    return render(request, 'usuario.html')
+
+def carrinho(request):
+    return render(request, 'carrinho.html')
